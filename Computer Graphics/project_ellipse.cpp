9fdaa369d6de35c5fd48cde00f8dae78f41302b6
Akiri Prabhanda <31247199+Prabhanda-Akiri@users.noreply.github.com>
2018-04-07 21:02:41 +0630

    Create project_ellipse.cpp

---
+++ Computer Graphics/project_ellipse.cpp
@@ -0,0 +1,304 @@
+#include<windows.h>
+#include<stdio.h>
+#include<GL/glut.h>
+
+GLfloat x1,y1,x2,y2,x3,y3,x4,y4,x5,y5;
+GLfloat xb1=600,yb1=10,xb2=680,yb2=10,xb3=700,yb3=60,xb4=580,yb4=60,xb5=580,yb5=60;
+
+GLint le[1000][20],re[1000][20];
+GLint ct[1000],cur_l[1000],cur_r[1000];
+
+GLfloat Xsize_s=0,Xsize_e=999;
+
+void drawpixel(int x,int y,float a,float b,float c)
+{
+            glColor3f(a,b,c);
+            glBegin(GL_POINTS);
+            glVertex2i(x,y);
+            glEnd();
+}
+
+void store_vertex(int x,int y)
+{
+            if(cur_l[y]<2 && cur_r[y]<2)
+                {
+                if(x<le[y][ct[y]])
+                {
+                    le[y][ct[y]]=x;
+                    cur_l[y]++;
+                }
+                if(x>re[y][ct[y]])
+                {
+                    re[y][ct[y]]=x;
+                    cur_r[y]++;
+                }
+                }
+                else
+                {
+                    ct[y]++;
+                    cur_l[y]=0;
+                    cur_r[y]=0;
+                if(x<le[y][ct[y]])
+                {
+                    le[y][ct[y]]=x;
+                    cur_l[y]++;
+                }
+                if(x>re[y][ct[y]])
+                {
+                    re[y][ct[y]]=x;
+                    cur_r[y]++;
+                }
+
+                }
+}
+
+void edgedetect(GLfloat x1,GLfloat y1,GLfloat x2,GLfloat y2)
+{
+            float mx,x,temp;
+            int i;
+            if((y2-y1)<0)
+            {
+              temp=x1;
+              x1=x2;
+              x2=temp;
+
+              temp=y1;
+              y1=y2;
+              y2=temp;
+            }
+
+            if((y2-y1)!=0)
+                mx=(x2-x1)/(y2-y1);
+            else
+                mx=x2-x1;
+
+            x=x1;
+
+            for(i=y1;i<y2;i++)
+            {
+                store_vertex(x,i);
+                x+=mx;
+            }
+}
+
+void detect_ellipse_up(double a,double b,float l,float st_x,float st_y,double a1,double b1,float l1,float st_x1,float st_y1,float u,float v,float w)
+{
+    int i,y,j;
+    for(i=0;i<1000;i++)
+    {
+       ct[i]=0;
+       cur_l[i]=0;
+       cur_r[i]=0;
+       for(j=0;j<20;j++)
+       {
+          le[i][j]=1000;
+          re[i][j]=0;
+       }
+    }
+
+    int sa=a*a;
+    int sb=b*b;
+    int ex=0;
+    int ey=b;
+
+    double d1=sb-(sa*b) + (0.5*sa) ;
+
+    store_vertex(ex+st_x,ey+st_y);
+
+    while((sa*(ey-0.5)>sb*(ex+1)) && ex<l1)
+    {
+        if(d1<0)
+            d1+=sb*3+sb*2*ex;
+        else
+        {
+            d1+=sb*3+sb*2*ex+2*sa-2*sa*ey;
+            ey--;
+        }
+        ex++;
+        store_vertex(-(ex)+st_x,ey+st_y);
+         store_vertex(ex+st_x,ey+st_y);
+    }
+
+    int le_y=ey+st_y,  le_x=ex+st_x,  le_x_l=-(ex)+st_x;
+    a=a1;
+    b=b1;
+    sa=a*a;
+    sb=b*b;
+    ex=0;
+    ey=b;
+
+    d1=sb-(sa*b) + (0.5*sa) ;
+
+    store_vertex(ex+st_x1,ey+st_y1);
+
+    while((sa*(ey-0.5)>sb*(ex+1)) && ex<l)
+    {
+        if(d1<0)
+            d1+=sb*3+sb*2*ex;
+        else
+        {
+            d1+=sb*3+sb*2*ex+2*sa-2*sa*ey;
+            ey--;
+        }
+        ex++;
+
+        store_vertex(le_x_l,ey+st_y1);
+        store_vertex(-(ex)+st_x1,ey+st_y1);
+        store_vertex(ex+st_x1,ey+st_y1);
+        store_vertex(le_x,ey+st_y1);
+    }
+
+    for(y=0;y<1000;y++)
+    {
+       for(i=0;i<ct[i]+1;i++)
+       {
+          if(le[y][i]<re[y][i])
+          {
+             for(j=le[y][i]+1;j<re[y][i];j++)
+                drawpixel(j,y,u,v,w);
+          }
+       }
+     }
+
+}
+
+void scanfill_poly(float x1,float y1,float x2,float y2,float x3,float y3,float x4,float y4,float x5,float y5,float a,float b,float c)
+{
+            int i,y,j;
+            for(i=0;i<1000;i++)
+            {
+                        ct[i]=0;
+                        cur_l[i]=0;
+                        cur_r[i]=0;
+                        for(j=0;j<20;j++)
+                        {
+                            le[i][j]=1000;
+                            re[i][j]=0;
+                        }
+            }
+
+            edgedetect(x1,y1,x2,y2);
+            edgedetect(x2,y2,x3,y3);
+            edgedetect(x3,y3,x4,y4);
+            edgedetect(x4,y4,x5,y5);
+            edgedetect(x5,y5,x1,y1);
+
+            for(y=0;y<1000;y++)
+            {
+                for(i=0;i<ct[i]+1;i++)
+                {
+                    if(le[y][i]<re[y][i])
+                    {
+                        for(j=le[y][i]+1;j<re[y][i];j++)
+                        drawpixel(j,y,a,b,c);
+                    }
+                }
+            }
+}
+
+void display()
+{
+            glColor3f(0.3,0.5,0.5);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            detect_ellipse_up(800,200,400,600,150,800,200,400,600,80,0.1,0.3,0.3);
+            //for the land mass
+            x1=0,y1=0,x2=300,y2=0,x3=200,y3=300,x4=0,y4=300,x5=0,y5=300;
+            scanfill_poly(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,0.5,0.1,0);
+
+            //for the tree bark
+            x1=90,y1=300,x2=110,y2=300,x3=110,y3=370,x4=90,y4=370,x5=90,y5=370;
+            scanfill_poly(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,0.3,0.1,0);
+            // first triangle in tree from bottom
+            x1=50,y1=370,x2=150,y2=370,x3=100,y3=440,x4=100,y4=440,x5=100,y5=440;
+            scanfill_poly(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,0.1,0.8,0.0);
+            // second triangle in tree from bottom
+            x1=60,y1=420,x2=140,y2=420,x3=100,y3=490,x4=100,y4=490,x5=100,y5=490;
+            scanfill_poly(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,0.1,0.8,0.0);
+            // third triangle in tree from bottom
+            x1=70,y1=470,x2=130,y2=470,x3=100,y3=520,x4=100,y4=520,x5=100,y5=520;
+            scanfill_poly(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,0.1,0.8,0.0);
+
+            // water level
+            x1=300,y1=0,x2=1000,y2=0,x3=1000,y3=9,x4=297,y4=9,x5=297,y5=9;
+            scanfill_poly(x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,0.0,0.0,0.8);
+
+            //boat base
+            scanfill_poly(xb1,yb1,xb2,yb2,xb3,yb3,xb4,yb4,xb5,yb5,0.3,0.1,0);
+
+
+
+   glFlush();
+   glutPostRedisplay();
+   glutSwapBuffers();
+}
+
+void translate_x(float x,float tx,int i)
+{
+    int flag=0;
+    x=x+tx;
+    if(x<Xsize_e)
+        flag=1;
+
+    switch(i)
+    {
+        case 1: xb1=x;break;
+        case 2: xb2=x;break;
+        case 3: xb3=x;break;
+        case 4: xb4=x;break;
+        case 5: xb5=x;break;
+    }
+
+    if(flag==1 && x>Xsize_e)
+    {
+         Xsize_s=Xsize_s+(x-Xsize_e);
+         Xsize_e=x;
+    }
+}
+
+void specialkey(int key,int x,int y)
+{
+    switch(key)
+    {
+    case GLUT_KEY_LEFT:
+        translate_x(xb1,-5,1);
+        translate_x(xb2,-5,2);
+        translate_x(xb3,-5,3);
+        translate_x(xb4,-5,4);
+        translate_x(xb5,-5,5);
+        glutPostRedisplay();
+        break;
+
+    case GLUT_KEY_RIGHT:
+        translate_x(xb1,5,1);
+        translate_x(xb2,5,2);
+        translate_x(xb3,5,3);
+        translate_x(xb4,5,4);
+        translate_x(xb5,5,5);
+        glutPostRedisplay();
+        break;
+    }
+
+}
+
+void myinit()
+{
+            glClearColor(0.3,0.5,0.5,1.0);
+            glColor3f(0.3,0.5,0.5);
+            glPointSize(1.0);
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            gluOrtho2D(Xsize_s,Xsize_e,0.0,999.0);
+}
+int main(int argc,char **argv)
+{           glutInit(&argc,argv);
+            glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
+            glutInitWindowSize(900,600);
+            glutInitWindowPosition(0,0);
+            glutCreateWindow("scanfill");
+            glutDisplayFunc(display);
+            myinit();
+            glutSpecialFunc(specialkey);
+            glutMainLoop();
+            return 0;
+   }
